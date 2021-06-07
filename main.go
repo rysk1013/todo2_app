57312bced79eecdb02b7986fa27b0916cf66bcff
rysk1013 <rysk2908@gmail.com>
2021-06-08 04:09:56 +0900

    test create tabel todos

--- main.go
+++ main.go
@@ -8,14 +8,7 @@ import (
 )
 
 func main() {
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.SQLDriver)
-	// fmt.Println(config.Config.DbName)
-	// fmt.Println(config.Config.LogFile)
-
-	// log.Println("test")
-
-	// fmt.Println(models.Db)
+	fmt.Println(models.Db)
 	
 	// u := &models.User{} 
 	// u.Name = "test"
@@ -24,16 +17,16 @@ func main() {
 	// fmt.Println(u)
 	// u.CreateUser()
 
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
+	// u, _ := models.GetUser(1)
+	// fmt.Println(u)
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
 
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 }
