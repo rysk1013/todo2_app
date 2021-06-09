97538fdb656658935d4163211baaeba3aceda196
rysk1013 <rysk2908@gmail.com>
2021-06-09 16:57:54 +0900

    test GetTodo

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ func main() {
 	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("First Todo")
+	// user, _ := models.GetUser(2)
+	// user.CreateTodo("First Todo")
+
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
 }
