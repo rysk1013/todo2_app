31316b18b64d3d0657f504d90c449cacce229d8f
rysk1013 <rysk2908@gmail.com>
2021-06-10 15:09:51 +0900

    create feature DeleteTodo

--- app/models/todos.go
+++ app/models/todos.go
@@ -104,3 +104,13 @@ func (t *Todo) UpdateTodo() error {
 	}
 	return err
 }
+
+func (t *Todo) DeleteTodo() (error) {
+	cmd := `DELETE FROM todos
+					WHERE id = ?`
+	_, err = Db.Exec(cmd, t.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
