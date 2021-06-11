3a6d32b72e947181d7d7fb864f2187387f7d5257
rysk1013 <rysk2908@gmail.com>
2021-06-12 00:18:33 +0900

    add navbar to func signup

--- app/controllers/user_auth.go
+++ app/controllers/user_auth.go
@@ -3,5 +3,5 @@ package controllers
 import "net/http"
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "signup")
+	generateHTML(w, nil, "layout", "public_navbar","signup")
 }
