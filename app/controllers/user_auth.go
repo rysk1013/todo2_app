453e94b168098bcb9f26db51562975eccb86aaca
rysk1013 <rysk2908@gmail.com>
2021-06-12 00:40:22 +0900

    in case POST to func signup

--- app/controllers/user_auth.go
+++ app/controllers/user_auth.go
@@ -1,7 +1,28 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"todo2_app/app/models"
+)
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar","signup")
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar","signup")
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		user := models.User{
+			Name: r.PostFormValue("name"),
+			Email: r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+		if err := user.CreateUser(); err != nil {
+			log.Fatalln(err)
+		}
+
+		http.Redirect(w, r, "/", 302)
+	}
 }
