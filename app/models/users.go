b316a8b21bf1eda92b02df735b1c2e6adc585fea
rysk1013 <rysk2908@gmail.com>
2021-06-13 03:40:31 +0900

    create func GetUserByEmail

--- app/models/users.go
+++ app/models/users.go
@@ -14,6 +14,14 @@ type User struct {
 	CreatedAt time.Time
 }
 
+type Session struct {
+	ID int
+	UUID string
+	Email string
+	UserID int
+	CreatedAt time.Time
+}
+
 func (u *User) CreateUser() (err error) {
 	cmd := `INSERT INTO users (
 			uuid,
@@ -38,9 +46,11 @@ func (u *User) CreateUser() (err error) {
 
 func GetUser(id int) (user User, err error) {
 	user = User{}
+
 	cmd := `SELECT id, uuid, name, email, password, created_at 
 					FROM users
 					WHERE id = ?`
+
 	err = Db.QueryRow(cmd, id).Scan(
 			&user.ID,
 			&user.UUID,
@@ -49,6 +59,7 @@ func GetUser(id int) (user User, err error) {
 			&user.PassWord,
 			&user.CreatedAt,
 	)
+
 	return user, err
 }
 
@@ -56,19 +67,42 @@ func (u *User) UpdateUser() (err error) {
 	cmd := `UPDATE users 
 					SET name = ?, email = ? 
 					WHERE id = ?`
+
 	_, err = Db.Exec(cmd, u.Name, u.Email, u.ID)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	return err
 }
 
 func (u *User) DeleteUser() (err error) {
 	cmd := `DELETE FROM users
 					WHERE id = ?`
+
 	_, err = Db.Exec(cmd, u.ID)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	return err
+}
+
+func GetUserByEmail(email string) (user User, err error) {
+	user = User{}
+
+	cmd := `SELECT id, uuid, name, email, password, created_at
+					FROM users
+					WHERE email = ?`
+
+	err = Db.QueryRow(cmd, email).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.PassWord,
+		&user.CreatedAt,
+	)
+
+	return user, err
 }
