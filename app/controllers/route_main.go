212b4a6144cff2285aa56cc9bb6df2c4a5a7a2fb
rysk1013 <rysk2908@gmail.com>
2021-06-13 06:19:27 +0900

    settings only login user

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -5,7 +5,12 @@ import (
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, "Hello", "layout", "public_navbar", "top")
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, "Hello", "layout", "public_navbar", "top")
+	} else {
+		http.Redirect(w, r, "/todos", 302)
+	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
